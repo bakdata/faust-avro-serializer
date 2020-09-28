4d4eeeb08443b168f532b2a5cbad96a2d09cf418
Alejandro Jaramillo <alejojaramillo96@gmail.com>
2020-09-28 13:28:06 +0200

    Use Azure pipelines for automatic release to PyPi (#7)

--- faust_avro_serializer/__init__.py
+++ faust_avro_serializer/__init__.py
@@ -1,6 +1,7 @@
 """Faust serializer that serializes Avro compatible messages"""
 
-__version__ = "1.0.2"
+__version__ = "1.0.1"
+
 
 from faust_avro_serializer.avro_serializer import FaustAvroSerializer
 from faust_avro_serializer.avro_serializer import MissingSchemaException
